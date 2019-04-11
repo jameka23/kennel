71c553fb17a5b06487deb2aa25b0db8984a3be64
Jameka Echols <jameka.echols@gmail.com>
2019-04-11 15:28:55 -0500

    added the location props to locationList.js

--- src/components/location/LocationList.js
+++ src/components/location/LocationList.js
@@ -4,19 +4,17 @@ import "./location.css"
 export default class LocationList extends Component {
     render() {
         return (
-            <React.Fragment>
+            <section>
                 <h1>Our Locations</h1>
-                <div className="locationWrapper">
-                <section className="locationContainer">
-                    <h4>Nashville North Location</h4>
-                    <h5>500 Puppy Way</h5>
-                </section>
-                <section className="locationContainer">
-                    <h4>Nashville South Location</h4>
-                    <h5>1600 Doggy Lane</h5>
-                </section>
-                </div>
-            </React.Fragment>
+                {
+                    this.props.locations.map(location => 
+                        <div key={location.id}>
+                            {location.name}
+                            {location.address}
+                        </div>
+                    )
+                }
+            </section>
         )
     }
 }
