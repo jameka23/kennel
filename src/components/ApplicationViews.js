692a78044525c9b7b774c5cdde9982f758d2f767
Jameka Echols <jameka.echols@gmail.com>
2019-04-12 14:56:49 -0500

    added json, locations and owners

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -9,40 +9,84 @@ import OwnerList from './owner/OwnerList'
 
 export default class ApplicationViews extends Component {
 
-    employeesFromAPI = [
-        { id: 1, name: "Jessica Younker" },
-        { id: 2, name: "Jordan Nelson" },
-        { id: 3, name: "Zoe LeBlanc" },
-        { id: 4, name: "Blaise Roberts" }
-    ]
+    // employeesFromAPI = [
+    //     { id: 1, name: "Jessica Younker" },
+    //     { id: 2, name: "Jordan Nelson" },
+    //     { id: 3, name: "Zoe LeBlanc" },
+    //     { id: 4, name: "Blaise Roberts" }
+    // ]
 
-    locationsFromAPI = [
-        { id: 1, name: "Nashville North", address: "500 Circle Way" },
-        { id: 2, name: "Nashville South", address: "10101 Binary Court" }
-    ]
+    // locationsFromAPI = [
+    //     { id: 1, name: "Nashville North", address: "500 Circle Way" },
+    //     { id: 2, name: "Nashville South", address: "10101 Binary Court" }
+    // ]
 
-    animalsFromAPI = [
-        { id: 1, name: "Doodles" },
-        { id: 2, name: "Jack" },
-        { id: 3, name: "Pork Chop" },
-        { id: 4, name: "Henley" },
-        { id: 5, name: "Derkins" },
-        { id: 6, name: "Checkers" }
-    ]
+    // animalsFromAPI = [
+        // { id: 1, name: "Doodles" },
+        // { id: 2, name: "Jack" },
+        // { id: 3, name: "Pork Chop" },
+        // { id: 4, name: "Henley" },
+        // { id: 5, name: "Derkins" },
+        // { id: 6, name: "Checkers" }
+    // ]
 
-    ownersFromAPI = [
-        { id: 1, name: "Jameka Echols", phoneNumber: "615-977-9837"},
-        { id: 2, name: "DeSean Webster", phoneNumber: "615-977-1111"},
-        { id: 3, name: "Delon Echols", phoneNumber: "615-977-2222"},
-        { id: 4, name: "Shawn Williams", phoneNumber: "615-977-3333"}
-    ]
+    // ownersFromAPI = [
+    //     { id: 1, name: "Jameka Echols", phoneNumber: "615-977-9837"},
+    //     { id: 2, name: "DeSean Webster", phoneNumber: "615-977-1111"},
+    //     { id: 3, name: "Delon Echols", phoneNumber: "615-977-2222"},
+    //     { id: 4, name: "Shawn Williams", phoneNumber: "615-977-3333"}
+    // ]
 
+    // state = {
+    //     employees: this.employeesFromAPI,
+    //     locations: this.locationsFromAPI,
+    //     animals: this.animalsFromAPI,
+    //     owners: this.ownersFromAPI
+    // }
+
+    // render() {
+    //     return (
+    //         <React.Fragment>
+    //             <Route exact path="/" render={(props) => {
+    //                 return <LocationList locations={this.state.locations} />
+    //             }} />
+    //             <Route path="/animals" render={(props) => {
+    //                 return <AnimalList animals={this.state.animals} />
+    //             }} />
+    //             <Route path="/employees" render={(props) => {
+    //                 return <EmployeeList employees={this.state.employees} />
+    //             }} />
+    //             <Route path="/owners" render={(props) => {
+    //                 return <OwnerList owners={this.state.owners} />
+    //             }} />
+    //         </React.Fragment>
+    //     )
+    // }
     state = {
-        employees: this.employeesFromAPI,
-        locations: this.locationsFromAPI,
-        animals: this.animalsFromAPI,
-        owners: this.ownersFromAPI
+        animals: [],
+        employees: [],
+        locations: [],
+        owners: []
     }
+
+    componentDidMount() {
+        const newState = {}
+
+        fetch("http://localhost:5002/animals")
+            .then(r => r.json())
+            .then(animals => newState.animals = animals)
+            .then(() => fetch("http://localhost:5002/employees")
+            .then(r => r.json()))
+            .then(employees => newState.employees = employees)
+            .then(() => fetch("http://localhost:5002/locations"))
+            .then(r => r.json())
+            .then(locations => newState.locations = locations)
+            .then(() => fetch("http://localhost:5002/owners"))
+            .then(r => r.json())
+            .then(owners => newState.owners = owners)
+            .then(() => this.setState(newState))
+    }
+
 
     render() {
         return (
